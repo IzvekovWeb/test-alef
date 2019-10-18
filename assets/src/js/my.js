1bbef8a79f0c8a84354bc64ef519c98014195602
Alexander <izvekov.web@gmail.com>
2019-10-18 12:32:45 +0300

    Верстка завершена на мобилку

--- assets/src/js/my.js
+++ assets/src/js/my.js
@@ -1,3 +1,19 @@
-$('.collapse').collapse({
-    toggle: true
-  })
+ 
+ 
+$(window).on('load resize',windowSize);
+$('body').on('load resize',windowSize);
+
+  function windowSize(){
+    if ( $('body').width() > '1024' || $(window).width() > '1024' ){ 
+      $('.collapse').collapse('show'); 
+    } else if( $('body').width() <= '1024' || $(window).width() <= '1024'){ 
+      $('.collapse').collapse('hide'); 
+    }
+}
+ 
+
+function showLinks(){   
+  var objJSON = JSON.parse(data);
+  console.log(data);
+}   
+showLinks();
